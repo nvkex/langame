6f612d12b9e4cbf8b0bb91952a0fb002cd56ad59
nvkex <scrbwrm@gmail.com>
2021-04-03 14:30:07 +0530

    Landing Page

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import classes from './LandingPage.module.css'
+
+const LandingPage = () => {
+    return (
+        <div className={classes.pageContainer}>
+            <div className={`container`}>
+                <div className="row">
+                    <div className="col-12">
+                        <h4 className="display-4 text-center">The Langame</h4>
+                    </div>
+                </div>
+                <div className={`row align-items-center ${classes.cardContainer}`}>
+                    <div className="col-6">
+                        <Link to="/guess">
+                            <div className={classes.card}>
+                                Guess
+                            </div>
+                        </Link>
+
+                    </div>
+                    <div className="col-6">
+                        <Link to="/learn">
+                            <div className={`${classes.disabled} ${classes.card}`}>
+                                Learn
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage
