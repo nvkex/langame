8579cc3597690600fadb3f3dc258dab633922a79
nvkex <scrbwrm@gmail.com>
2021-04-04 11:32:59 +0530

    add Mode Container routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import { Route, Switch } from 'react-router';
 import './App.css';
 import LandingPage from './components/LandingPage/LandingPage';
+import ModeContainer from './containers/ModeContainer';
+import GuessMode from './components/GameModes/Guess/GuessMode';
+import LearnMode from './components/GameModes/Learn/LearnMode';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={LandingPage} />
+        <ModeContainer>
+          <Switch>
+            <Route exact path="/guess" component={GuessMode}/>
+            <Route exact path="/learn" component={LearnMode}/>
+          </Switch>
+        </ModeContainer>
       </Switch>
     </div>
   );
 }
 
 export default App;
+
+
+/*
+https://picsum.photos/
+https://portal.clarifai.com/users/j8vnv4pavgbw/apps/4d00ea9a8e5c46d794705a4fb30f7aa2/model-mode
+https://github.com/Clarifai/clarifai-nodejs-grpc
+*/
