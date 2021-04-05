32fe28cd4a032b20afb83bd970f853312ca84dc4
nvkex <scrbwrm@gmail.com>
2021-04-05 21:46:40 +0530

    Picsum constants

--- src/constants/URLs.js
+++ src/constants/URLs.js
@@ -1,3 +1,8 @@
 
 export const RANDOM_WORD_URL = 'http://names.drycodes.com/1?nameOptions=objects&combine=1';
 export const CORS_BYPASS_URL = 'https://uncors.now.sh/?url=';
+
+// Guess Mode
+const WIDTH = 300;
+const HEIGHT = 200;
+export const PICSUM_URL = `https://picsum.photos/${WIDTH}/${HEIGHT}?random=`;
