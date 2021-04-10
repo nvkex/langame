64b87af34c1c083df1c74b92f2e7d0f184852877
nvkex <scrbwrm@gmail.com>
2021-04-10 17:37:40 +0530

    Learn Mode Message

--- src/components/GameModes/Learn/LearnMode.js
+++ src/components/GameModes/Learn/LearnMode.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const LearnMode = () => {
   return (
-    <div>
-      Learn Mode
+    <div className="container">
+      <div className="display-1 text-center text-light" style={{fontWeight:600}}>
+        Coming Soon...
+      </div>
     </div>
   )
 }
