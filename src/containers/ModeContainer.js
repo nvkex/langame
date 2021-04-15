48653e8de3a8018ec24c5f95b705c4c15c5105d6
nvkex <scrbwrm@gmail.com>
2021-04-15 15:57:42 +0530

    add hometab and dev link

--- src/containers/ModeContainer.js
+++ src/containers/ModeContainer.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import DevMsg from '../components/SmallComponents/DevMsg'
+import HomeTab from '../components/SmallComponents/HomeTab'
 
 const ModeContainer = (props) => {
   return (
-    <div>
+    <div style={{height: '100%'}}>
+      <HomeTab />
       {props.children}
+      <DevMsg />
     </div>
   )
 }
