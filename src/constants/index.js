c6c631ae0689ec47a4b0c4c31598f3082b0a655f
nvkex <scrbwrm@gmail.com>
2021-04-15 15:57:29 +0530

    increase time limit

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,7 +4,7 @@ export const CORS_BYPASS_URL = 'https://uncors.now.sh/?url=';
 
 // Guess Mode
 export const MAX_STAGE = 11;
-export const TIME_LIMIT = 20;
+export const TIME_LIMIT = 25;
 
 export const WIDTH = 300;
 export const HEIGHT = 200;
@@ -13,3 +13,4 @@ export const PICSUM_URL2 = `https://picsum.photos/seed/`;
 // Backend URLs
 export const BASE_URL = 'http://langameapiv1-env.eba-bgpkxq8p.ap-southeast-1.elasticbeanstalk.com/';
 export const DETECT_OBJECTS = 'get-objects/';
+export const DETECT_OBJECTS_TR = 'get-objects-tr/';
