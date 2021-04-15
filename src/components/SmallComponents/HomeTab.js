a3c21de91086eec326ae8adcf7db6ab6943b4594
nvkex <scrbwrm@gmail.com>
2021-04-15 15:57:06 +0530

    Home link button

---
+++ src/components/SmallComponents/HomeTab.js
@@ -0,0 +1,12 @@
+import classes from './HomeTab.module.css'
+import React from 'react'
+
+const HomeTab = () => {
+  return (
+    <a href="/" className={classes.hometab}>
+      &#127968; Home
+    </a>
+  )
+}
+
+export default HomeTab
