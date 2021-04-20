33499e6d94907ebc22b9382ba4bd6df709e998d1
nvkex <scrbwrm@gmail.com>
2021-04-20 14:47:43 +0530

    mobile fixed bottom bar

--- src/components/SmallComponents/HomeTab.js
+++ src/components/SmallComponents/HomeTab.js
@@ -4,7 +4,8 @@ import React from 'react'
 const HomeTab = () => {
   return (
     <a href="/" className={classes.hometab}>
-      &#127968; Home
+      &#127968; 
+      <span className={classes.text}>Home</span>
     </a>
   )
 }
