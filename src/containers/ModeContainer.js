879a9fa7459b150336424d9cbe9f3c4f83470dc7
nvkex <scrbwrm@gmail.com>
2021-04-20 14:48:21 +0530

    add:classes to children

--- src/containers/ModeContainer.js
+++ src/containers/ModeContainer.js
@@ -1,13 +1,17 @@
+import classes from './ModeContainer.module.css'
 import React from 'react'
 import DevMsg from '../components/SmallComponents/DevMsg'
 import HomeTab from '../components/SmallComponents/HomeTab'
 
 const ModeContainer = (props) => {
   return (
-    <div style={{height: '100%'}}>
-      <HomeTab />
-      {props.children}
-      <DevMsg />
+    <div>
+      <div className={classes.children}>{props.children}</div>
+      <div className={classes.fixedComponents}> 
+        <HomeTab />
+        <DevMsg />
+      </div>
+
     </div>
   )
 }
