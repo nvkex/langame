5792ceda1ec51ea69f6d41f4509ae2fb5ce7464c
nvkex <scrbwrm@gmail.com>
2021-04-21 12:59:20 +0530

    Split App info into different component

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import AppInfo from '../../common/AppInfo/AppInfo'
 import classes from './LandingPage.module.css'
 
 const LandingPage = () => {
     return (
-        <div className={classes.pageContainer}>
+        <div style={{marginTop:'4px'}} className={classes.pageContainer}>
             <div className={`container`}>
-                <div className="row">
-                    <div className="col-12">
-                        <h4 className="display-3 text-center">The Langame</h4>
-                    </div>
-                </div>
+                <AppInfo />
                 <div className={`row align-items-center ${classes.cardContainer}`}>
                     <div className="col-12 col-md-12 col-lg-6">
                         <Link to="/guess">
