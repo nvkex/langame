ca88e5f8a8ec5e08d9fcc0fe9b0df70930e7077c
nvkex <scrbwrm@gmail.com>
2021-04-21 12:59:52 +0530

    Add logo and fix responsive faults

--- src/components/SmallComponents/SelectLanguage.js
+++ src/components/SmallComponents/SelectLanguage.js
@@ -2,21 +2,24 @@ import classes from './SelectLanguage.module.css'
 import React from 'react'
 import TextCard1 from '../../common/Cards/TextCard1'
 import { SUPPORTED_LANGUAGES } from '../../data/Languages'
+import AppInfo from '../../common/AppInfo/AppInfo'
 
 const SelectLanguage = (props) => {
   return (
-    <div className={classes.container}>
-      <h2 className="display-4 text-light text-center">Select a language.</h2>
-      <div className={`row`}>
-        {
-          SUPPORTED_LANGUAGES.map(language => (
-            <div className="col-3 col-lg-3" onClick={() => props.setLanguage(language)}>
-              <TextCard1 text={language.lang} />
-            </div>
-          ))
-        }
+    <div className="container">
+      <AppInfo />
+      <div className={classes.container}>
+        <h2 className="display-4 text-light text-center">Select a language.</h2>
+        <div className={`row`}>
+          {
+            SUPPORTED_LANGUAGES.map(language => (
+              <div className="col-6 col-lg-3" onClick={() => props.setLanguage(language)} key={language.code}>
+                <TextCard1 text={language.lang} />
+              </div>
+            ))
+          }
+        </div>
       </div>
-
     </div>
   )
 }
