e54613d7a00a2e91a1011ffd7c20035f0ea82539
nvkex <scrbwrm@gmail.com>
2021-04-21 12:25:38 +0530

    Supported Language Data

---
+++ src/data/Languages.js
@@ -0,0 +1,18 @@
+export const SUPPORTED_LANGUAGES = [
+    {
+        lang: 'French',
+        code: 'fr'
+    },
+    {
+        lang: 'German',
+        code: 'fr'
+    },
+    {
+        lang: 'Swedish',
+        code: 'fr'
+    },
+    {
+        lang: 'Japanesse',
+        code: 'fr'
+    }
+]
