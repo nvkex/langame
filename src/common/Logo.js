dcdc3848c7dcd9c343275a67c8fb510dfd1364cc
nvkex <scrbwrm@gmail.com>
2021-04-22 15:02:53 +0530

    padding update

--- src/common/Logo.js
+++ src/common/Logo.js
@@ -4,8 +4,8 @@ import { DEFAULT_LOGO_WIDTH } from '../constants'
 
 const Logo = (props) => {
   return (
-    <div style={{width: props.width ? props.width : DEFAULT_LOGO_WIDTH}} className={classes.logoContainer}>
-        <img style={{width: props.width ? props.width : DEFAULT_LOGO_WIDTH}} className={`img-fluid ${classes.logo}`}src="./static/images/logo.png" alt="logo"/>
+    <div style={{width: props.width ? props.width : DEFAULT_LOGO_WIDTH, paddingTop:'6px'}} className={classes.logoContainer}>
+        <img style={{width: props.width ? props.width : DEFAULT_LOGO_WIDTH}} className={`img-fluid ${classes.logo}`}src="./assets/images/logo.png" alt="logo"/>
     </div>
   )
 }
