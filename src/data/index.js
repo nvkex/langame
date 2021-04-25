613416e1c0146cd81dacf2f60b602839ad696b29
nvkex <scrbwrm@gmail.com>
2021-04-25 08:43:49 +0530

    rename

---
+++ src/data/index.js
@@ -0,0 +1,39 @@
+export const SUPPORTED_LANGUAGES = [
+    {
+        lang: 'French',
+        code: 'fr'
+    },
+    {
+        lang: 'German',
+        code: 'ge'
+    },
+    {
+        lang: 'Swedish',
+        code: 'sw'
+    },
+    {
+        lang: 'Japanesse',
+        code: 'jp'
+    }
+]
+
+export const themeConfig = {
+    dark: {
+        color: 'white',
+        color2: 'black',
+        secondary: '#c6d5e3',
+        primaryRed: '#fc6d6d',
+        secondaryRed: '#fc1b1b',
+        primaryGreen: '#99f4be',
+        secondaryGreen: '#18bb5c'
+    },
+    light: {
+        color: 'white',
+        color2: 'black',
+        secondary: '#6c757d',
+        primaryRed: '#c02626',
+        secondaryRed: '#c45858',
+        primaryGreen: '#198754 ',
+        secondaryGreen: '#18bb5c'
+    }
+}
