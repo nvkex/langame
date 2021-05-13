1919ba8099c1934a371b3943ca5cec84341594fa
nvkex <scrbwrm@gmail.com>
2021-05-13 14:13:57 +0530

    Improved customizability of component

--- src/common/Cards/TextCard1.js
+++ src/common/Cards/TextCard1.js
@@ -2,18 +2,27 @@ import React from 'react'
 import styled from 'styled-components'
 
 const TextCard = styled.div`
-background-color: white;
-color: black;
+background-color: ${props => props.bg || "white"};
+color: ${props => props.textColor || "black"};
+border: ${props => props.border || "none"};
 padding: 16px 10px;
 border-radius: 10px;
 margin: 4px 0;
 cursor: pointer;
 font-weight: 700;
+font-family: 'Roboto Condensed', sans-serif;
+font-size: ${props => props.textSize || "1rem"};
 `;
 
 const TextCard1 = (props) => {
   return (
-    <TextCard className={`text-center`}>
+    <TextCard
+      className={`text-center`}
+      textSize={props.textSize}
+      bg={props.bg}
+      textColor={props.textColor}
+      border={props.border}
+      >
       {props.text}
     </TextCard>
   )
